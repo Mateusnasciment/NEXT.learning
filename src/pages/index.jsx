08c454bf254a1ff70a007a8067e70f9643535a07
Mateus nascimento <mateus.nasciment@hotmail.com>
2022-09-30 20:17:44 -0300

    função em JSX

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,17 @@
 export default function Início () {
-  return "início"
+  const titulo = <h1> titulo no jsx</h1>
+
+  function subtitulo() {
+    
+    return <h2>{"função dentro da função".TopUpperCase()}</h2>
+  }
+  return (
+    <div> 
+        {titulo}
+        {subtitulo}
+        <p>
+          {JSON.stringify({nome: 'Mateus', idade: 23})}
+        </p>
+    </div>
+    )
 }
